f056ae823fc0d5d9b96e006a532c57056994e321
finntenzor <finntenzor@gmail.com>
2019-05-05 23:44:48 +0800

    init dev-frontend

---
+++ dev-frontend/src/reports-backend/store.js
@@ -0,0 +1,16 @@
+import Vue from './node_modules/vue'
+import Vuex from './node_modules/vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
